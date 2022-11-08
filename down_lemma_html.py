29059177f47aae8ee9b9cf66e6f72d938ed84cba
yzqzss <yzqzss@yandex.com>
2022-11-08 23:25:00 +0800

    下载条目 html 纯文本的脚本

---
+++ down_lemma_html.py
@@ -0,0 +1,19 @@
+import json
+from http_get_request import *
+import os
+
+with open('data/lemmaId_set.json', 'r') as f:
+    lemmaId_set = set(json.load(f))
+
+if os.path.exists('data/kexue/') == False:
+    os.mkdir('data/kexue/')
+
+for i in lemmaId_set:
+    print(i)
+    time.sleep(0.5)
+    # https://baike.sogou.com/kexue/d69917841985450255.htm
+    r = http_get_request(f'https://baike.sogou.com/kexue/d{i}.htm')
+    with open(f'running.html', 'w') as f:
+        f.write(r.text)
+    with open(f'data/kexue/d{i}.html', 'w') as f:
+        f.write(r.text)
