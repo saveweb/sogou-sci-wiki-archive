10c61568e47755a25901d135b8e5803dc0057389
yzqzss <yzqzss@yandex.com>
2022-11-08 23:26:03 +0800

    推送条目 url 到 IA 的脚本

---
+++ push_lemma_url_to_IA.py
@@ -0,0 +1,15 @@
+import json
+import spn2api
+spn2api.LOAD_API_AUTH('LOW $accesskey:$secret')
+
+with open('data/lemmaId_set.json', 'r') as f:
+    lemmaId_set = json.load(f)
+
+count=0
+for lemmaId in lemmaId_set:
+    count+=1
+    print(count,'/',len(lemmaId_set),':',lemmaId)
+    # https://baike.sogou.com/kexue/d69917841985450255.htm
+
+    url = f'https://baike.sogou.com/kexue/d{lemmaId}.htm'
+    spn2api.do_archive(url)
