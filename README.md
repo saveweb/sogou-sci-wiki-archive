45f2a6873f47923cd6a126895ebd8ef2d847f71e
yzqzss <yzqzss@yandex.com>
2023-10-16 20:01:21 +0800

    fin

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # sogou-sci-wiki-archive
 
-搜狗科学百科 (http://baike.sogou.com) 存档
+搜狗科学百科 <http://baike.sogou.com> 存档
 
 页面存档列表见：[archived_page.md](./archived_page.md) 
--- push_lemma_url_to_IA.py
+++ push_lemma_url_to_IA.py
@@ -1,6 +1,6 @@
 import json
 import spn2api
-spn2api.LOAD_API_AUTH('LOW $accesskey:$secret')
+spn2api.LOAD_API_AUTH('LOW ACC:SEC')
 
 with open('data/lemmaId_set.json', 'r') as f:
     lemmaId_set = json.load(f)
@@ -8,6 +8,8 @@ with open('data/lemmaId_set.json', 'r') as f:
 count=0
 for lemmaId in lemmaId_set:
     count+=1
+    if count<= 180:
+        continue
     print(count,'/',len(lemmaId_set),':',lemmaId)
     # https://baike.sogou.com/kexue/d69917841985450255.htm
 
